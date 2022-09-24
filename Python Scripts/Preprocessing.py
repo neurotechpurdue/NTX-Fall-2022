344d0d6581c4e0eb7fcf436236023400aa776632
tyler <tylerjmorgan3@gmail.com>
2022-09-24 14:58:36 -0400

    Code from summer project

--- Python Scripts/Preprocessing.py
+++ Python Scripts/Preprocessing.py
@@ -24,4 +24,33 @@ data_stamp = 'Testing Recordings\BrainFlow-RAW_2022-09-17_15-44-49_0.csv'
 data = pd.read_csv(data_path, skiprows=1)
 chnames = ['ch1', 'ch2', 'ch3', 'ch4', 'Timestamps']
 
-#a 
+sampling_freq = 200  # in Hertz
+chtypes = ['eeg', 'eeg', 'eeg', 'eeg', 'stim']
+info = mne.create_info(chnames, sfreq=sampling_freq)
+
+
+
+raw = mne.io.RawArray(data.T, info)
+raw.plot(duration=1, highpass=0.01, lowpass=62.4)
+raw.plot(n_channels=1, duration=1,show_scrollbars=False, show_scalebars=False)
+
+
+stamps = pd.read_csv(data_stamp, skiprows=1)
+
+start = stamps.loc[0][1]
+onset = []
+description=[]
+duration=[]
+#for all timestamps value, subtract start and add to onset
+for x in range(len(stamps)):
+  description.append(stamps.loc[x][0])
+  ms = stamps.loc[x][1] - start
+  onset.append(ms/1000) #why did i use math.floor??
+  duration.append(0) 
+print(onset)
+print(description)
+
+#
+annotations = mne.Annotations(onset=onset, duration=duration, description=description)
+raw.set_annotations(annotations)
+# raw.plot(duration=onset[len(onset) -1], highpass=0.01, lowpass=30)
